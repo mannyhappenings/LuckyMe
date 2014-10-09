452012d62524189884d0a95f6d6a2cf3ff299781
Rohit Kumar <rohit2ism@gmail.com>
2014-10-09 18:34:28 +0530

    Updated code for Python 3 with backward compatibility
    Updated for Python 3 which is also compatible for Python 2.
    Also verifying input from user

--- LuckyMe.py
+++ LuckyMe.py
@@ -2,20 +2,22 @@ from random import randint
 from math import pow
 
 # Ask for luck index(How lucky you feel today)
-print "Please set you luck index, (1 or 2 or 3 or 4),"
-n = raw_input()
+print ("Please set you luck index: (1 or 2 or 3 or 4)")
+n = input()
+n = int(n)
+
+# Check input validity
+while(not(n==1 or n==2 or n==3 or n==4)):
+        print("You have entered invalid input, Please enter (1 or 2 or 3 or 4)")
+        n = input()
+        n = int(n)
 
 # Set the according to luck index given by user
-n = int(pow(10, int(n)))
-
-
-# If the user enter 0
-if(n==1):
-	n=2
+n = int(pow(10, n))
 
 # Show chances of the winning
 # Calculated as 1 - (chances of loosing)
-print "You will %f probabilty to win. So, let try your luck." % (1- pow((float(n-1)/n), 4))
+print ("You have %f probabilty to win. So, let try your luck." % (1- pow((float(n-1)/n), 4)))
 
 # 'choice' decides Till when the following loop will run(depends on users choice to play again)
 a = True
@@ -27,26 +29,26 @@ while a:
 		l.append(randint(0, n-1))
 
 	# Ask for an input
-	print "Input a integer(0 to %d): " % (n-1)
-	a = raw_input()
+	print ("Input a integer(0 to %d): " % (n-1))
+	a = input()
 	a = int(a)
 
 	# Show if the user won or not
 	if a in l:
-		print "You win!! :-)"	
+		print ("You win!! :-)")	
 	else:
-		print "You lose :-("
+		print ("You lose :-(")
 	# Show user the list for transparency
-	print l
+	print (l)
 
 	# Ask for playing again...
-	print "Wanna try again??, y to try your luck again: "
-	a = raw_input()
-	if(a=="y"):
+	print ("Wanna try again??, y to try your luck again: ")
+	a = input()
+	if(a=="y" or a=="Y"):
 		a =  True
 	else:
 		a = False
 	pass
 
 # Print greetings for playing
-print "Thank you for playing this game :)"
+print ("Thank you for playing this game :)")
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ The game starts with asking for a luck index.
 Luck index is how lucky you feel today.
 
 For example if give 1 as your lucky index,
-the you will be asked to give a number between 0 and 9 inclusive
+then you will be asked to give a number between 0 and 9 inclusive
 and four random numbers are generated.
 If any of the four numbers is your number, then you win else you lose.
 
-For making this process more transparent, we also show the random list generted.
+For making this process more transparent, we also show the random list generated.
